5cd656d40b81cb9a63d516ea938c9de52a58f56a
AFFU-star <affaanaulia@gmail.com>
2024-08-07 10:38:50 +0530

    Add files via upload
    This  is  a  java  code   for  displaying  the  needed  pattern  using  arrays  with  space  of  2  characters  in between .

---
+++ Patternarr.java
@@ -0,0 +1,36 @@
+//  The  goal  is  to  display  this  using  arrays  :
+/*
+ 
+ 
+0  0  0  0  0  0  0  0  0  0 
+ 0  0  0  0  0  0  0  0  0  0 
+ 0  0  0  0  0  0  0  0  0  0 
+ 0  0  0  0  0  0  0  0  0  0 
+ 0  0  0  0  0  0  0  0  0  0 
+ 0  0  0  0  0  0  0  0  0  0 
+ 0  0  0  0  0  0  0  0  0  0 
+ 0  0  0  0  0  0  0  0  0  0 
+ 0  0  0  0  0  0  0  0  0  0 
+ 0  0  0  0  0  0  0  0  0  0 
+*/
+
+// Two  dimensional  array with rows and columns.
+
+// Define a class named Main.
+public class Patternarr {
+    // The main method where the program execution starts.
+    public static void main(String[] args) {   
+        // Declare a two-dimensional integer array 'a' with dimensions 10x10.
+        int[][] a = new int[10][10];    
+        
+        // Use nested loops to iterate over each row and column of the array.
+        for (int i = 0; i < 10; i++)  {
+            for (int j = 0; j < 10; j++) {
+                // Print each element of the array, ensuring a minimum width of 2 characters.
+                System.out.printf("%2d ", a[i][j]);
+            }
+            // Move to the next line to represent a new row in the output.
+            System.out.println();
+        }
+    }
+}
